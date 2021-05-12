00fa645d92857c5769b47ac82f6fdf1dca4aa21c
Philip Adams <pbadams@uchicago.edu>
2021-05-12 16:40:28 -0500

    bsmm_test: send matrix to GPU ahead of measurement

--- bsmm-op/bsmm_test.py
+++ bsmm-op/bsmm_test.py
@@ -6,10 +6,10 @@ with tf.device('GPU:0'):
     #class BSMMTest(tf.test.TestCase):
         #def testBSMM(self):
     bsmm_module = tf.load_op_library("./bsmm.so")
-    bsmm = bsmm_module.BCSRMatMulNA
+    bsmm = bsmm_module.BCSRMatMul
     matrix_size = 2048
     block_size = 16
-    blocks_per_row = 64
+    blocks_per_row = 16
     rows = int(matrix_size/block_size)
 
     blocks = np.array([np.random.rand(block_size,block_size) for i in range(blocks_per_row*rows)])
@@ -18,7 +18,12 @@ with tf.device('GPU:0'):
     for i in range(rows):
         np.append(columns,np.random.choice(np.array(range(rows)), blocks_per_row))
     dense = np.random.rand(matrix_size, matrix_size)
-
+    
+    blocks = tf.convert_to_tensor(blocks)
+    columns = tf.convert_to_tensor(columns)
+    row_ptr = tf.convert_to_tensor(row_ptr)
+    dense = tf.convert_to_tensor(dense)
+    
     start = time.perf_counter_ns()
     output = bsmm(block_size=block_size, col_ids=columns, row_ptr=row_ptr, blocks=blocks, dense=dense)
     elapsed = time.perf_counter_ns() - start
